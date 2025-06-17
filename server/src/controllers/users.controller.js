5fb9cf8cfb8db71ba9e9dc64255fc8f72d0a09f4
sabts <sabrinataraschi@gmail.com>
2025-06-17 09:58:05 +0200

    .

--- server/src/controllers/users.controller.js
+++ server/src/controllers/users.controller.js
@@ -2,8 +2,65 @@ const usersController = {};
 const UserModel = require("../models/user.model");
 
 usersController.getAllUsers = async (req, res) => {
-  const allUser = await UserModel.find();
-  res.send(allUser);
+  try {
+    const allUser = await UserModel.find();
+    res.send(allUser);
+  } catch (error) {
+    res.status(500).send("Error getting user", error);
+  }
+};
+
+usersController.getUserById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const userFound = await UserModel.findById(id);
+
+    if (!userFound) {
+      res.status(404).send({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: "Error gettin user Id" + error });
+  }
+};
+
+usersController.createUser = async (req, res) => {
+  const newUser = new UserModel({ ...req.body });
+  try {
+    await newUser.save();
+    const allUser = await UserModel.find();
+    res.status(200).send(allUser);
+  } catch (error) {
+    res.status(500).send({ message: "Error creating an user" + error });
+  }
+};
+
+usersController.updateUser = async (req, res) => {
+  const { id } = req.params;
+  // const newData = req.body;
+
+  try {
+    await UserModel.updateOne({ _id: id }, { $set: { ...req.body } });
+    //aqui le pedimos que busque el id que coincide con el _id y de ahi hacemos un set de los cambios
+    const allUser = await UserModel.find();
+    res.status(200).send(allUser);
+    // const userFound = await UserModel.findById(id);
+    // if (!userFound) {
+    //   res.status(404).send({ message: "User not found" });
+    // }
+  } catch (error) {
+    res.status(404).send({ message: "Error uptdating user" + error });
+  }
+};
+
+usersController.deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await UserModel.deleteOne({ _id: id });
+    const allUser = await UserModel.find();
+    res.status(200).send(allUser);
+  } catch (error) {
+    res.status(500).send({ message: "Error deleting user" + error });
+  }
 };
 
 module.exports = usersController;
--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -4,5 +4,9 @@ const usersController = require("../controllers/users.controller");
 const userRoutes = express.Router();
 
 userRoutes.get("/", usersController.getAllUsers);
+userRoutes.get("/id", usersController.getUserById);
+userRoutes.post("/", usersController.createUser);
+userRoutes.patch("/id", usersController.updateUser);
+userRoutes.delete("/id", usersController.deleteUser);
 
 module.exports = userRoutes;
